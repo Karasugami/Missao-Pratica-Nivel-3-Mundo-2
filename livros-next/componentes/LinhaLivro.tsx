9284b90ddee8157c08e67bff773e93d1c5b4c9b0
Davison Rodrigues <davisrodrigues1@gmail.com>
2024-10-22 15:31:21 -0300

    checkpoint

--- livros-next/componentes/LinhaLivro.tsx
+++ livros-next/componentes/LinhaLivro.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { ControleEditora } from '../classes/controle/ControleEditora';
 import { Livro } from '../classes/modelo/Livro';
 
-// a) Instância de ControleEditora
 const controleEditora = new ControleEditora();
 
-// b) Interface para os props do componente
+
 interface LinhaLivroProps {
     livro: Livro;
     excluir: () => void;
 }
 
-// c) Definindo o componente exportável
 export const LinhaLivro: React.FC<LinhaLivroProps> = (props) => {
     const { livro, excluir } = props;
     const nomeEditora = controleEditora.getNomeEditora(livro.codEditora);
